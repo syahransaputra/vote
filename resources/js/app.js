a623314c7e9709bccd6fa96340d78e39c2f98af0
Syahran <syahran.saputra@gmail.com>
2023-03-19 02:05:39 +0800

    Setting template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+/*
+This file will be used by jetstream to add alpine.js. This file must exist to install jetstream successfully.
+You can remove it if you don't want to use jetstream.
+*/
